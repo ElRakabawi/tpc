806fb6ca51e26648ad3176584f0e28abeff3278d
EC2 Default User <ec2-user@ip-172-31-47-115.us-west-2.compute.internal>
2017-12-20 16:26:28 +0000

    Router.js removed and migrated to SPA-architecture

--- tpc-core/src/main.js
+++ tpc-core/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './components/App.vue'
-import router from './js/router.js'
 import Vuetify from 'vuetify'
 import Toolbar from './components/Toolbar.vue'
 import Auth from './components/Auth.vue'
+import Inst from './components/Inst.vue'
 import '../node_modules/vuetify/dist/vuetify.min.css';
 import "./js/blockchain.js";
 
@@ -24,7 +24,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el:"v-app",
-  router,
   components: {
     App
   }
